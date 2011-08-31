7b80e915c329aa256c0c079b356f2b8a5eb73257
Sachin Nair <rsachinnair@gmail.com>
2011-08-31 09:35:14 +0530

    Chapter 1 complete Signed-off-by: Sachin Nair <rsachinnair@gmail.com>

--- SudokuVer1/src/org/example/sudoku/SudokuActivity.java
+++ SudokuVer1/src/org/example/sudoku/SudokuActivity.java
@@ -1,13 +1,23 @@
 package org.example.sudoku;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class SudokuActivity extends Activity implements OnClickListener{
-    /** Called when the activity is first created. */
+
+	private static final String TAG = "Sudoku";
+	
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +45,46 @@ public class SudokuActivity extends Activity implements OnClickListener{
 			Intent i = new Intent(this,About.class);
 			startActivity(i);
 			break;
+			
+		case R.id.new_button:
+			openNewGameDialog();
+			break;
+			
+		case R.id.exit_button:
+			finish();
+			break;
 		}
-		
+	}
+	
+	private void openNewGameDialog(){
+		new AlertDialog.Builder(this).setTitle(R.string.new_game_title).setItems(R.array.difficulty, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface arg0, int i) {
+				startGame(i);
+			}
+		}).show();
+	}
+	
+	private void startGame(int i){
+		Log.d(TAG,"clicked on " + i);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+			super.onCreateOptionsMenu(menu);
+			MenuInflater inflater = getMenuInflater();
+			inflater.inflate(R.menu.menu, menu);
+			return true;			
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+		case R.id.settings:
+			startActivity(new Intent(this,Prefs.class));
+			return true;
+		}
+		return false;
 	}
 }
