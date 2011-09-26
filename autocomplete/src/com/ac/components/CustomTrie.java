361419bb37f87a3ae6016a03bf11386a32b790e3
theincredibleguy <rsachinnair@gmail.com>
2011-09-26 15:45:12 +0530

    testing commit
    Signed-off-by: theincredibleguy <rsachinnair@gmail.com>

--- autocomplete/src/com/ac/components/CustomTrie.java
+++ autocomplete/src/com/ac/components/CustomTrie.java
@@ -1,5 +1,7 @@
 package com.ac.components;
 
+import java.util.ArrayList;
+
 /**
  * represents a trie structure trie structure with message id as root and every string of the message as branch e.g for a message HEY THERE following will be the structure
  * 						
@@ -23,5 +25,20 @@ package com.ac.components;
 public class CustomTrie {
 	
 	private int msgId;
-	CharSequence[] branch;
+	private ArrayList<ArrayList> branches ;
+	
+	public CustomTrie() {
+		branches = new ArrayList<ArrayList>();
+	}
+	public int getMsgId() {
+		return msgId;
+	}
+	public void setMsgId(int msgId) {
+		this.msgId = msgId;
+	}
+	
+	public void addBranch(String branch){
+		//TODO to convert String to a branch of chars -- might have to reimplement CharSequence
+//		branches.add(branch);
+	}
 }
