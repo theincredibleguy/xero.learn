8d436cd47cd3e282a78819445b974a4f0d3489df
sachin <rsachinnair@gmail.com>
2011-09-26 18:48:31 +0530

    committing remote changes to local
    Signed-off-by: sachin <rsachinnair@gmail.com>

---
+++ autocomplete/src/com/ac/components/Leaf.java
@@ -0,0 +1,23 @@
+package com.ac.components;
+
+public class Leaf implements CharSequence {
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public char charAt(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- autocomplete/src/com/ac/util/TrieFactory.java
+++ autocomplete/src/com/ac/util/TrieFactory.java
@@ -3,7 +3,6 @@ package com.ac.util;
 import java.util.StringTokenizer;
 
 import com.ac.components.CustomTrie;
-import com.ac.components.L1Hash;
 
 /**
  * this factory will create a CustomTrie out of a String message
@@ -12,14 +11,13 @@ import com.ac.components.L1Hash;
  */
 public class TrieFactory {
 	
-	public static L1Hash[] generateTrie(String message){
-		CustomTrie ct = createTrie(message);
-		
-		return null;
+	public static CustomTrie generateTrie(int messageId, String message){
+		CustomTrie ct = new CustomTrie();
+		ct.setMsgId(messageId);
+		StringTokenizer st = new StringTokenizer(message);
+		while(st.hasMoreElements()){
+			ct.addBranch(st.nextToken());
+		}
+		return ct;
 	}
-	
-	private static CustomTrie createTrie(String message){
-		return null;
-	}
-
 }
